c1d30906f614d01ceaa78e3a7080bb418721e538
Lautaro Cozzani <lautaromail@gmail.com>
2018-02-06 14:08:50 -0300

    avoid streams getting list of matches

--- routes/matches.js
+++ routes/matches.js
@@ -7,7 +7,7 @@ exports.config = function(app) {
         if (req.query.owner !== undefined && req.query.owner !== null && req.query.owner !== '') {
             filter.owner = req.query.owner;
         }
-        model.Match.find(filter)
+        model.Match.find(filter, '-streams')
             .populate('owner')
             .sort('-startDate')
             .then(matches => res.send(matches))
